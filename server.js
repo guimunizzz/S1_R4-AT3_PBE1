a750fe9fa7607856e84057532e298d81f69cb59d
guimunizzz <guilherme.muniz5@hotmail.com>
2025-10-09 10:55:49 -0300

    starting exercise2

--- server.js
+++ server.js
@@ -4,32 +4,9 @@ const PORT = 8081;
 
 app.use(express.json()); // para trabalhar com json no express é necessario essa expressão
 
-async function validaNumeros(pNotas) {
-    if (pNotas.some(elemento => typeof elemento != 'number')) {
-        throw new Error('Revise o array de média');
-    }
-    return pNotas;
-}
-
-async function mediaNotas(pNotas) {
-    const notasValidadas = await validaNumeros(pNotas);
-    const soma = notasValidadas.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0);
-
-    return soma / notasValidadas.length;
-}
-
-async function aprovacaoNotas(mediaNotas) {
-    if (mediaNotas > 6) {
-        return "aprovada";
-    }
-    return "aeprovada"
-}
-
-app.post('/alunos', async (req,res) => {
+app.post('/soma', async (req,res) => {
     try {
-        const {nome, notas} = req.body;
-        const resultadoMedia = await mediaNotas(notas);
-        const resultadoAprovacao = await aprovacaoNotas(resultadoMedia)
+        
 
         res.status(201).json({resultado: `Olá ${nome}, o resultado da media de suas notas é: ${resultadoMedia}, e você foi ${resultadoAprovacao}!`})
     } catch (error) {
